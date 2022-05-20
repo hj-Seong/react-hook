eaed08df092dbb8b0b6dcb4a834fe72bcdec3831
hjseong <hjseong1222@gmail.com>
2022-05-20 16:11:06 +0900

    reducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ExReducer from "./components/ExReducer";
+import HookComp from "./components/HookComp";
+import ReducerComp2 from "./components/ReducerComp2";
+import ReduserComp from "./components/ReduserComp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HookComp />
+      <ReduserComp />
+      <ReducerComp2 />
+      <ExReducer />
     </div>
   );
 }
