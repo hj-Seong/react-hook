23b002b2739a5226e63a2554381b0c2bf6205eb7
hjseong <hjseong1222@gmail.com>
2022-05-24 10:52:55 +0900

    Memohook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import ExReducer from "./components/ExReducer";
 import HookComp from "./components/HookComp";
+import MemoHook from "./components/MemoHook";
 import ReducerComp2 from "./components/ReducerComp2";
 import ReduserComp from "./components/ReduserComp";
 
 function App() {
   return (
     <div className="App">
+      <MemoHook />
       <HookComp />
       <ReduserComp />
       <ReducerComp2 />
