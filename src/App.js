b8eef4fbac40afd81e12d3d35721217f1655f150
hjseong <hjseong1222@gmail.com>
2022-05-25 12:33:06 +0900

    context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
+import React from "react";
+import { ThemeContext } from "./components/ThemeContext";
 import ExReducer from "./components/ExReducer";
 import HookComp from "./components/HookComp";
 import MemoHook from "./components/MemoHook";
 import ReducerComp2 from "./components/ReducerComp2";
 import ReduserComp from "./components/ReduserComp";
+import ContextHook from "./components/ContextHook";
 
 function App() {
   return (
     <div className="App">
+      <ThemeContext.Provider value={"light"}>
+        <ContextHook />
+      </ThemeContext.Provider>
+
       <MemoHook />
       <HookComp />
       <ReduserComp />
